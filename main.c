65d75e063849e7143ce5dd3661f06adea3c11105
EVAOTA <nervevawaka@ezweb.ne.jp>
2017-02-26 14:06:24 +0900

    first commit seed barnch

---
+++ main.c
@@ -0,0 +1,38 @@
+/*
+ * File:   main.c
+ * Author: evaota
+ *
+ * Created on 2017/02/25, 21:19
+ */
+
+
+#include <xc.h>
+#include "main.h"
+
+int8_t main(void)
+{
+    Basic_Init();
+
+    return EXIT_SUCCESS;
+}
+
+int8_t Basic_Init(void)
+{
+    OSCCON = 0xF3;      //PLL ON: 
+                        //internal Oscilltor is 32MHz
+                        //clock source is internal one.
+    
+    TRISA = 0x07;       //RA0~2 is input it is used CPS  
+    ANSELA = 0x07;      //RA0~2 is analog it is used CPS
+   
+    TRISB = 0x40;       //RA6 is input it is used Timer1
+    ANSELB = 0x00;      //All PORTB pin is digital
+    /*alternate pin fuction control*/
+    APFCON0 = 0x08;     //RA7 use CCP2
+    APFCON1 = 0x00;     //no alteration
+    
+    PORTA = 0x00;       //clear
+    PORTB = 0x00;       //clear
+    
+    return EXIT_SUCCESS;
+}
---
+++ main.h
@@ -0,0 +1,53 @@
+/* 
+ * File:   main.h
+ * Author: evaota
+ *
+ * Created on 2017/02/25, 21:19
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#include <stdlib.h>
+#include <stdint.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+// PIC16F1827 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1
+#pragma config FOSC = INTOSC    // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
+#pragma config WDTE = ON        // Watchdog Timer Enable (WDT enabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable (PWRT enabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+#pragma config IESO = OFF       // Internal/External Switchover (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config PLLEN = ON       // PLL Enable (4x PLL enabled)
+#pragma config STVREN = OFF     // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will not cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+    
+#define _XTAL_FREQ 32000000
+    
+int8_t Basic_Init(void);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
